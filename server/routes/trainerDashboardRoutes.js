7c059cce7cb6b0f62b83a3527570ed2cc8dacb38
N.Chanuuj(NC) <chanuujnavaretnam@gmail.com>
2026-01-28 08:59:11 +0530

    Delete server directory

--- server/routes/trainerDashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { protect, trainerOnly } = require("../middleware/auth");
-
-const { getTrainerDashboardSummary } = require("../controllers/trainerDashboardController");
-
-router.get("/dashboard/summary", protect, trainerOnly, getTrainerDashboardSummary);
-
-module.exports = router;
